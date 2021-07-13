f4ad9a0c26a2cf0fcb56c624618cd8e773119681
riedahoosain <rhoosain@gmail.com>
2021-07-13 23:43:53 +0200

    commit

--- etch-a-sketch-start/main.py
+++ etch-a-sketch-start/main.py
@@ -4,10 +4,31 @@ tim = Turtle()
 screen = Screen()
 
 
+def reset_drawing():
+    tim.reset()
+
+
 def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
+def move_counter_clockwise():
+    tim.right(10)
+
+
+def move_clockwise():
+    tim.left(10)
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "space")
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=reset_drawing)
 screen.exitonclick()
